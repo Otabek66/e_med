0bf0182bb993c9bc39278c1a0116f0f1139eb63a
Otabek66 <otabekotaboev@icloud.com>
2022-05-25 13:59:17 +0500

    Widgets, routes, screens and core[constants,components]

---
+++ lib/screens/auth/view/splash_view.dart
@@ -0,0 +1,16 @@
+import 'package:e_med/core/constants/color/ColorConst.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorConst.kPrimaryColor,
+      body: Center(child: ),
+    );
+  }
+}
---
+++ lib/widgets/textform_filed_widget.dart
@@ -0,0 +1,36 @@
+import 'package:e_med/core/constants/color/ColorConst.dart';
+import 'package:flutter/material.dart';
+
+class MyTextField {
+  static textField(
+      {required String text,
+      IconButton? iconButton,
+      required TextEditingController controller,
+      IconButton? phoneNumber,
+      bool read = false,
+      var onChanged,
+      VoidCallback? onTap}) {
+    return TextFormField(
+      controller: controller,
+      readOnly: read,
+      onChanged: onChanged,
+      onTap: onTap,
+      decoration: InputDecoration(
+        hintText: text,
+        hintStyle: const TextStyle(color: Colors.grey),
+        suffixIcon: iconButton,
+        prefixIcon: phoneNumber,
+        fillColor: Colors.white,
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          borderSide: const BorderSide(color: ColorConst.kPrimaryColor),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+      ),
+    );
+  }
+}
