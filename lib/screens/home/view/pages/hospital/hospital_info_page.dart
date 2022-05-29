40464c1fca3dc3fc8673052fccba300fe5ef8bf4
Otabek66 <otabekotaboev@icloud.com>
2022-05-30 01:21:27 +0500

    Some problems solved

--- lib/screens/home/view/pages/hospital/hospital_info_page.dart
+++ lib/screens/home/view/pages/hospital/hospital_info_page.dart
@@ -24,9 +24,10 @@ class HospitalInfoView extends StatelessWidget {
         body: SafeArea(
       child: SizedBox(
         height: context.h,
-        child: Column(
-          children: [
-            AppBarWidget(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              AppBarWidget(
                 trailing: IconConst.filter,
                 leading: InkWell(
                   child: Row(children: [Icon(Icons.chevron_left)]),
@@ -34,43 +35,65 @@ class HospitalInfoView extends StatelessWidget {
                     context.read<HomeCubit>().changeState(HospitalState());
                   },
                 ),
-                center: TextWidget.textwidget("Hospital")),
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage(info.image),
-            ),
-            TextWidget.textwidget(info.name),
-            Container(
-              width: context.w * 0.893,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    title: const Text("Phone number"),
-                    subtitle: Text(info.phoneNumber),
-                    trailing: Image.asset("assets/icons/phone.png"),
-                  ),
-                  listTile("Working time",
-                      Text("${info.workingDay} \n${info.workingHour}")),
-                  listTile(
-                    "Location Link",
-                    Text(
-                      info.locationLink,
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                  listTile(
-                    "Website",
-                    Text(
-                      info.locationLink,
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                  TextWidget.textwidget("Doctors at Hospital")
-                ],
+                center: TextWidget.textwidget("Hospital"),
               ),
-            )
-          ],
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage(info.image),
+              ),
+              TextWidget.textwidget(info.name),
+              Container(
+                width: context.w * 0.893,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: const Text("Phone number"),
+                      subtitle: Text(info.phoneNumber),
+                      trailing: Image.asset("assets/icons/phone.png"),
+                    ),
+                    listTile("Working time",
+                        Text("${info.workingDay} \n${info.workingHour}")),
+                    listTile(
+                      "Location Link",
+                      Text(
+                        info.locationLink,
+                        style: const TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    listTile(
+                      "Website",
+                      Text(
+                        info.locationLink,
+                        style: const TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    TextWidget.textwidget("Doctors at Hospital"),
+                    SizedBox(
+                      height: 300,
+                      child: ListView.builder(
+                        itemBuilder: (ctx, ind) {
+                          return ListTile(
+                            trailing: InkWell(
+                              child: Icon(Icons.chevron_right),
+                              onTap: () {},
+                            ),
+                            leading: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage(info.dInfo[ind].image),
+                            ),
+                            title: Text(info.dInfo[ind].name),
+                            subtitle: Text(info.dInfo[ind].spes),
+                          );
+                        },
+                        itemCount: info.dInfo.length,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ));
